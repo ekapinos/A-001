21481221e2fbac00ada17fa34f95867c03cad429
Evgeniy Kapinos <evgeniy.kapinos@gmail.com>
2016-05-16 21:22:21 +0300

    Added annotated JMX bean

---
+++ 20-JMX/src/main/java/local/kapinos/beans/_03_ExposingByAnnotationsBean.java
@@ -0,0 +1,41 @@
+package local.kapinos.beans;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ManagedResource(objectName="myFolder:name=_03_ExposingByAnnotationsBean")
+public class _03_ExposingByAnnotationsBean {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	private int intValue = 20;
+	
+	@ManagedAttribute
+	public int getIntValue() {
+		logger.info("getIntValue()");
+		return intValue;
+	}
+	
+	@ManagedAttribute
+	public void setIntValue(int intValue) {
+		logger.info("setIntValue(" + intValue + ")");
+		this.intValue = intValue;
+	}
+
+	@PostConstruct
+	public void start()  {
+		logger.info("Exposing by Annotations Bean started");
+	}
+	
+	@PreDestroy
+	public void stop()  {
+		logger.info("Exposing by Annotations Bean stopped");
+	}	
+}
--- 20-JMX/src/main/java/local/kapinos/config/SpringConfiguration.java
+++ 20-JMX/src/main/java/local/kapinos/config/SpringConfiguration.java
@@ -7,12 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
 import org.springframework.jmx.export.assembler.InterfaceBasedMBeanInfoAssembler;
+import org.springframework.jmx.export.assembler.MetadataMBeanInfoAssembler;
 import org.springframework.jmx.export.assembler.MethodNameBasedMBeanInfoAssembler;
 
 import local.kapinos.beans._00_BeansMarkerInterface;
 import local.kapinos.beans._01_ExposingByMethodNamesBean;
 import local.kapinos.beans._02_ExposingByInterfaceBean;
+import local.kapinos.beans._03_ExposingByAnnotationsBean;
 import local.kapinos.common.WaitAnyKeyBean;
 
 @Configuration
@@ -21,34 +24,41 @@ public class SpringConfiguration {
 
 	@Bean
 	public MBeanExporter methodNameMbeanExporter(_01_ExposingByMethodNamesBean myBean) {
-		MBeanExporter exporter = new MBeanExporter();
-		Map<String, Object> beans = new HashMap<String, Object>();
-		beans.put("myFolder:name=_01_myBean", myBean);
-		exporter.setBeans(beans);
-		exporter.setAssembler(methodNameAssembler());
-		return exporter;
-	}
-	@Bean
-	public MethodNameBasedMBeanInfoAssembler methodNameAssembler() {
 		MethodNameBasedMBeanInfoAssembler assembler = new MethodNameBasedMBeanInfoAssembler();
 		assembler.setManagedMethods(new String[] { "getIntValue", "setIntValue" });
-		return assembler;
+
+		MBeanExporter exporter = new MBeanExporter();
+		Map<String, Object> beans = new HashMap<String, Object>();
+		beans.put("myFolder:name=_01_ExposingByMethodNamesBean", myBean);
+		exporter.setBeans(beans);
+		exporter.setAssembler(assembler);
+		return exporter;
 	}
 
 	@Bean
 	public MBeanExporter interfaceMbeanExporter(_02_ExposingByInterfaceBean myBean) {
-		MBeanExporter exporter = new MBeanExporter();
-		Map<String, Object> beans = new HashMap<String, Object>();
-		beans.put("myFolder:name=_02_myBean", myBean);
-		exporter.setBeans(beans);
-		exporter.setAssembler(interfaceAssembler());
-		return exporter;
-	}
-	@Bean
-	public InterfaceBasedMBeanInfoAssembler interfaceAssembler() {
 		InterfaceBasedMBeanInfoAssembler assembler = new InterfaceBasedMBeanInfoAssembler();
 		assembler.setManagedInterfaces(new Class<?>[] { _02_ExposingByInterfaceBean.class });
-		return assembler;
+ 
+		MBeanExporter exporter = new MBeanExporter();
+		Map<String, Object> beans = new HashMap<String, Object>();
+		beans.put("myFolder:name=_02_ExposingByInterfaceBean", myBean);
+		exporter.setBeans(beans);
+		exporter.setAssembler(assembler);
+		return exporter;
+	}
+	
+	@Bean
+	public MBeanExporter annotationMbeanExporter(_03_ExposingByAnnotationsBean myBean) {
+		MetadataMBeanInfoAssembler assembler = new MetadataMBeanInfoAssembler();
+		assembler.setAttributeSource(new AnnotationJmxAttributeSource());
+
+		MBeanExporter exporter = new MBeanExporter();
+		Map<String, Object> beans = new HashMap<String, Object>();
+		beans.put("myFolder:name=_03_ExposingByAnnotationsBean", myBean);
+		exporter.setBeans(beans);
+		exporter.setAssembler(assembler);
+		return exporter;
 	}
 	
 	@Bean
